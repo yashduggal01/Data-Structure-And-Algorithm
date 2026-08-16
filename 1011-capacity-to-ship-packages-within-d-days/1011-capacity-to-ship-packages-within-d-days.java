cbf7438405f25274df16724be9cdf436ab8cb6ef
Yash Duggal <158084730+yashduggal01@users.noreply.github.com>
2026-08-16 17:18:01 +0530

    Time: 10 ms (83.41%), Space: 50.3 MB (20.24%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,38 +1,41 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int low = 1;
-        int total = 0;
-        for(int i = 0 ;i<weights.length;i++){
-            total+=weights[i];
-            low = Math.max(low,weights[i]);
+        int n=weights.length;
+        int max=0;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            if(weights[i]>max){
+                max=weights[i];
+            }
         }
-        int high = total;
-        int ans = high;
+        for(int i=0;i<n;i++){
+            sum=sum+weights[i];
+        }
+        int low=max;
+        int high=sum;
         while(low<=high){
-            int mid = low+(high-low)/2;
-            if(feasible(weights,days,mid)){
-                ans = mid;
-                high = mid-1;
+            int mid=(high+low)/2;
+            int ndays=f(weights,mid);
+            if(ndays<=days){
+                high=mid-1;
             }
             else{
-                low = mid+1;
+                low=mid+1;
             }
         }
-        return ans;
+        return low;
     }
-    static boolean feasible(int[] arr , int days , int cap){
-        int curr = 0;
-        int total = 1;
-        for(int num : arr){
-            if(num>cap) return false;
-            if(curr+num>cap){
-                curr = num;
-                total++;
+    public static int f(int[] weights,int mid){
+        int days=1,load=0;
+        for(int i=0;i<weights.length;i++){  
+            if(load+weights[i]>mid){
+                days=days+1;
+                load=weights[i];
             }
             else{
-                curr+=num;
+                load+=weights[i];
             }
         }
-        return total<=days;
+        return days;
     }
 }
