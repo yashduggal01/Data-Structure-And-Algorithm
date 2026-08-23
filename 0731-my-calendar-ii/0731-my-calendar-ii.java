e9f0fa6576481b16bdec907fdf554ba04c4e5413
Yash Duggal <158084730+yashduggal01@users.noreply.github.com>
2026-08-23 18:27:18 +0530

    Time: 44 ms (91.74%), Space: 47.3 MB (69.15%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.java
+++ 0731-my-calendar-ii/0731-my-calendar-ii.java
@@ -1,27 +1,31 @@
+import java.util.*;
+
 class MyCalendarTwo {
-    TreeMap<Integer,Integer> map;
+    private List<int[]> bookings;
+    private List<int[]> overlaps;
+
     public MyCalendarTwo() {
-        map = new TreeMap<>();
+        bookings = new ArrayList<>();
+        overlaps = new ArrayList<>();
     }
     
     public boolean book(int startTime, int endTime) {
-        map.put(startTime,map.getOrDefault(startTime,0)+1);
-        map.put(endTime,map.getOrDefault(endTime,0)-1);
-        int count = 0;
-        for(int key : map.keySet() ){
-            count+=map.get(key);
-            if(count>2){
-                map.put(startTime,map.getOrDefault(startTime,0)-1);
-                map.put(endTime,map.getOrDefault(endTime,0)+1);
+        for (int[] doubleBooked : overlaps) {
+            if (Math.max(startTime, doubleBooked[0]) < Math.min(endTime, doubleBooked[1])) {
                 return false;
             }
         }
+        
+        for (int[] event : bookings) {
+            if (Math.max(startTime, event[0]) < Math.min(endTime, event[1])) {
+                overlaps.add(new int[]{
+                    Math.max(startTime, event[0]), 
+                    Math.min(endTime, event[1])
+                });
+            }
+        }
+        
+        bookings.add(new int[]{startTime, endTime});
         return true;
     }
 }
-
-/**
- * Your MyCalendarTwo object will be instantiated and called as such:
- * MyCalendarTwo obj = new MyCalendarTwo();
- * boolean param_1 = obj.book(startTime,endTime);
- */
