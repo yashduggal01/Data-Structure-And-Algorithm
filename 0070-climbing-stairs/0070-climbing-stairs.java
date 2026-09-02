199c80145866e9aea51a09e5bc94cbf7c028019d
Yash Duggal <158084730+yashduggal01@users.noreply.github.com>
2026-09-02 22:00:57 +0530

    Time: 0 ms (100%), Space: 42.3 MB (17.43%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    static int[] dp;
+    public int climbStairs(int n) {
+        if(n<=1) return 1;
+        dp = new int[n+1];
+        //Arrays.fill(dp,-1);
+        dp[0]=1;
+        dp[1] =1;
+        for(int i =2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+    // static int solve(int n){
+    //     if(n<=1) return 1;
+    //     if(dp[n]!=-1) return dp[n];
+    //     return dp[n] = solve(n-1)+solve(n-2);
+    // }
+}
